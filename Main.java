00bb2cd912e7b1e12fe574fefb7d5a3744ee3c09
SeyitGunes <86159276+SeyitGunes@users.noreply.github.com>
2023-08-30 22:21:13 +0300

    HesapMakinesi
    Java ile yazilmis hesap makinesi

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int n1,n2,select;
+        Scanner input = new Scanner(System.in);
+        System.out.println("ilk sayıyı giriniz: ");
+        n1 = input.nextInt();
+        System.out.println("ikinci sayıyı giriniz");
+        n2 = input.nextInt();
+
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.print("Seçiminiz: ");
+        select = input.nextInt();
+
+        switch (select){
+            case 1:
+                System.out.println(" = : " + (n1 + n2));
+                break;
+            case 2:
+                System.out.println(" = : " + (n1 - n2));
+                break;
+            case 3:
+                System.out.println(" = : " + (n1 * n2));
+                break;
+            case 4:
+                System.out.println(" = : " + (n1 / n2));
+                break;
+            default:
+                System.out.println("Yanlış seçim yaptınız!!! Tekrar Deneyiniz");
+                break;
+        }
+    }
+    }
+
